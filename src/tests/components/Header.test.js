666a143ae9f5d1244a9dc898fb9f22b82dfa0877
eversharp <eversharp@eversharpdeMacBook-Pro.local>
2020-07-18 15:47:53 -0500

    DB auth

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-//import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
-import toJson from 'enzyme-to-json';
-test('shoul', () => {
-  const wrapper = shallow(<Header/>);
-  expect(toJson(wrapper)).toMatchSnapshot();
+import { Header } from '../../components/Header';
 
-})
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header startLogout={() => { }} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
